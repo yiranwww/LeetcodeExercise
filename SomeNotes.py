5bc49fd9a1c52b1622444b17114d7f6c0dc81844
yiranwww <37821405+yiranwww@users.noreply.github.com>
2025-02-23 17:53:35 -0800

    Update and rename SoneNotes.py to SomeNotes.py

--- SomeNotes.py
+++ SomeNotes.py
@@ -8,3 +8,13 @@ Stack.is_empty() >> len(List) == 0
 ## Array
 S[start: stop] >> [Start, stop)
 S[start: Stop : step]
+
+
+## stack and queue
+stack = [1,2,3,4]
+stack.pop() # 4
+stack = [1,2,3]
+
+queue = [1,2,3,4]
+queue.pop() # 1
+queue = [2,3,4]
