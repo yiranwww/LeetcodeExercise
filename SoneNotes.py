a19d0d98f50fd9fa7a0d66aa2f5748df052ea3d8
yiranwww <37821405+yiranwww@users.noreply.github.com>
2025-02-23 16:58:43 -0800

    Create SoneNotes.py

---
+++ SoneNotes.py
@@ -0,0 +1,10 @@
+## Stack
+Stack.push(e) >> List.append(e)
+Stack.pop() >> List.pop()
+Stack.top() >> List[-1]
+Stack.is_empty() >> len(List) == 0
+
+
+## Array
+S[start: stop] >> [Start, stop)
+S[start: Stop : step]
